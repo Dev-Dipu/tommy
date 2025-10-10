725c7eaa4914ba230333c18367d21e4065f926ae
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-10-11 01:09:38 +0530

    cards added and minor changes in ORB

--- components/CardScreen.jsx
+++ components/CardScreen.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const CardScreen = () => {
   return (
-    <div className='h-screen'>
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex gap-8">
+        <div
+          className="card1 relative flex items-center justify-center w-full h-full bg-no-repeat bg-center bg-contain"
+          style={{ backgroundImage: "url('./images/cardbgDesign.svg')" }}
+        >
+          <img
+            src="./images/servicescard.svg"
+            alt="cards"
+            className="w-78 h-auto"
+          />
+        </div>
+        
+        <div
+          className="card2 relative flex items-center justify-center w-full h-full bg-no-repeat bg-center bg-contain"
+          style={{ backgroundImage: "url('./images/cardbgDesign.svg')" }}
+        >
+          <img
+            src="./images/servicescard.svg"
+            alt="cards"
+            className="w-78 h-auto"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardScreen
+export default CardScreen;
